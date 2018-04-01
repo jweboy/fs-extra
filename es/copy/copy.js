bf46039a036fdea4c6aaf59bdfe95dd46c9fbd2f
jianglei <jweboy0630@gmail.com>
2018-04-01 23:47:27 +0800

    add: startCopy function and update logic

--- es/copy/copy.js
+++ es/copy/copy.js
@@ -1,14 +1,25 @@
-import { pathExist } from '../path-exist'
+import { pathAccessAsync } from '../path-access'
 
-export default function copy(src, dist, opts, cb) { 
-  console.log('copy', pathExist());
-  return new Promise((resolve, reject) => { 
-    pathExist(dist)
-      .then((exist) => { 
-        console.log(exist);
-      })
-      .catch(err => {
-        reject(err)
-      })
+function _startCopy(...args) { 
+  console.log('start-copy', ...args);
+}
+
+export default  function copy(src, dist, opts) { 
+  return new Promise(async (resolve, reject) => { 
+    try { 
+      const distAccessError = await pathAccessAsync(dist)
+      // console.log('copy', distAccessError)
+      if (!distAccessError) { 
+        // dist is exist => start copy
+        return _startCopy(src, dist, opts)
+      }
+      throw distAccessError
+      // if (!distAccessError) {
+      // }
+    } catch (err) {
+      // dist isn't exist => mkdir
+      // mkdirs()
+      console.log('mkdir', err);
+    }
   })
 }
--- test.js
+++ test.js
@@ -1,9 +1,16 @@
 import path from 'path'
 import fs from './es'
 
-fs.copy('', path.resolve('./es1'), {}, function (err) {
-}).then(() => { 
+fs.copy('', path.resolve('./es'), {})
+  .then(() => { 
   
-}).catch(err => { 
-  console.log(1, err);
-})
+  }).catch(err => { 
+    console.log('copy-err', err);
+  })
+
+// fs.pathAccessAsync(path.resolve('./es'))
+// .then((data) => { 
+//   console.log('ok', data);
+// }).catch(err => { 
+//   console.log('exist-err', err);
+// })
