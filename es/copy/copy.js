bfdc9c0ac27c3300f109a87b2c2e430e01913c85
jianglei <jweboy0630@gmail.com>
2018-04-05 20:47:48 +0800

    update: copy with copydir  function

--- es/copy/copy.js
+++ es/copy/copy.js
@@ -4,6 +4,10 @@ import { pathAccessAsync } from '../path-access'
 import { mkdirAsync } from '../mkdir'
 // import { promiseify } from '../util/promiseify'
 
+const NOTEXIST = Symbol('notExist')
+const EXIST = Symbol('exist')
+const ENCODEING = 'utf-8'
+
 function _startCopy (...args) {
   return new Promise(async function (resolve, reject) {
     try {
@@ -16,19 +20,97 @@ function _startCopy (...args) {
 
 function _getStats (...args) {
   return new Promise(async function (resolve, reject) {
-    console.log('start-copy', ...args)
+    // console.log('start-copy', ...args)
 
     const [src, dist] = args
-    const stat = fs.lstatSync
-
-    console.log(src)
+    const statSync = fs.lstatSync
+    // console.log(src)
 
     try {
-      await stat(src)
+      const stat = await statSync(src)
+      // 目录类型
+      if (stat.isDirectory()) {
+        await _onDir(...args)
+      }
+      // 文件类型
+      if (stat.isFile() ||
+        stat.isBlockDevice() ||
+        stat.isCharacterDevice()
+      ) {
+        _onFile()
+      }
     } catch (err) {
       reject(err)
     }
   })
+}
+
+const readlinkAsync = (path, opts = {
+  encoding: ENCODEING
+}) => new Promise(function (resolve, reject) {
+  fs.readlink(path, opts, function (err, linkstring) {
+    if (err) {
+      // TODO: windows may throw UNKNOWN error
+      // dest exists and is a regular file or directory
+      if (err.code === 'EINVAL') {
+        resolve(EXIST)
+      }
+      if (err.code === 'ENOENT') {
+        resolve(NOTEXIST)
+      } else {
+        reject(err)
+      }
+    }
+    resolve(linkstring)
+  })
+})
+
+const _checkDist = async (src, dist) => new Promise(async function (resolve, reject) {
+  try {
+    const linkPath = await readlinkAsync(dist)
+    if (linkPath === NOTEXIST) {
+      if (_isSrcSubDir(src, dist)) {
+        reject(new Error(`Cannot copy ${src} to a subdirectory of itself, ${dist}`))
+      }
+    }
+    if (linkPath === EXIST) {
+      if (_isSrcSubDir(src, dist)) {
+        reject(new Error(`Cannot copy ${src} to a subdirectory of itself, ${dist}`))
+      }
+      _mapCopyDir()
+    }
+  } catch (err) {
+    console.log('checkdir-err', err)
+  }
+})
+
+function _mapCopyDir() { 
+  
+}
+
+async function _onDir (src, dist) {
+  console.log('copy dir', dist)
+  try {
+    const codeVal = await _checkDist(src, dist)
+    console.log('codeVal', codeVal)
+  } catch (err) {
+    console.log('ondir-err', err)
+  }
+}
+
+function _isSrcSubDir (src, dist) {
+  const srcArray = src.split(path.sep)
+  const distArray = dist.split(path.stp)
+
+  srcArray.reduce(function (res, curr, index) {
+    return res && distArray[index] === curr
+  }, true)
+
+  // console.log(srcArray, distArray)
+}
+
+function _onFile () {
+  console.log('copy file')
 }
 
 /**
@@ -45,6 +127,7 @@ export default function copy (src, dist, opts) {
       reject(new Error('src and dist must not be the same.'))
     }
     try {
+      dist = path.dirname(dist)
       const distIsExist = await pathAccessAsync(dist)
       console.log('distIsExist', distIsExist)
       if (distIsExist) {
